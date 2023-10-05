2184d6595081f6dee1bb9966c8e4255234e684c4
Juan Pablo Moreno <juanmor@unisabana.edu.co>
2023-10-05 16:40:10 -0500

    Actualización

--- UniSabana/core/views.py
+++ UniSabana/core/views.py
@@ -1,9 +1,19 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "core/index.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        message = request.POST.get('message')
+        response = 'Hi, this is my response'
+        return JsonResponse({'message': message, 'response': response})
+
 
 class SingInPageView(TemplateView):
     template_name = "core/signin.html"
