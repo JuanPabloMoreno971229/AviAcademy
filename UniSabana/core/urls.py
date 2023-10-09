c822105fa0d8fe9ad8ba6d4a271622f4b6af5160
Juan Pablo Moreno <juanmor@unisabana.edu.co>
2023-10-09 10:41:05 -0500

    Integración ChatGPT

--- UniSabana/core/urls.py
+++ UniSabana/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, SingInPageView, SingOutPageView
+from .views import HomePageView, SingInPageView, SingOutPageView, ChatPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="Home"),
     path("signin/", SingInPageView.as_view(), name="SignIn"),
     path("signout/", SingOutPageView.as_view(), name="SingOut"),
+    path("chat-avi/", ChatPageView.as_view(), name="ChatAvi"),
 ]
--- UniSabana/core/views.py
+++ UniSabana/core/views.py
@@ -1,23 +1,59 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 from django.http import JsonResponse
+import openai
+
+#openai_api_key = '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'
+#openai.api_key = openai_api_key
+
+
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "core/index.html"
-    
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
-
-    def post(self, request, *args, **kwargs):
-        message = request.POST.get('message')
-        response = 'Hi, this is my response'
-        return JsonResponse({'message': message, 'response': response})
-
 
 class SingInPageView(TemplateView):
     template_name = "core/signin.html"
 
 class SingOutPageView(TemplateView):
     template_name = "core/signout.html"
+
+class ChatPageView(TemplateView):
+    template_name = "core/chat.html"
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        message = request.POST.get('message')
+
+        try:
+            # Llamar a la función ask_openai para obtener una respuesta de OpenAI
+            response = "Aqui estoy"#self.ask_openai(message)
+
+            # Devolver la respuesta en un JSON
+            return JsonResponse({'message': message, 'response': response})
+        except Exception as e:
+            # Manejar cualquier excepción que pueda ocurrir y devolver un mensaje de error
+            return JsonResponse({'error': str(e)})
+
+    # def ask_openai(self, message):
+    #     print("hola")
+    #     try:
+    #         response = openai.Completion.create(
+    #             model="text-davinci-003",
+    #             prompt=message,
+    #             max_tokens=20,
+    #             n=1,
+    #             stop=None,
+    #             temperature=0.7,
+    #         )
+    #         print(message)
+    #         print("Bien")
+    #         answer = response.choices[0].text.strip()
+    #         print(response)
+    #         return answer
+    #     except Exception as e:
+    #         # Manejar cualquier excepción que pueda ocurrir al interactuar con OpenAI
+    #         return f"Error: {str(e)}"
+        
 
