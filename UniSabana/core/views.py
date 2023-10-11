36126dc42c34253221540bb1e40c74b5f3b81506
sebastianrima <sebastianrima@unisabana.edu.co>
2023-10-11 11:34:23 -0500

    fixing some bugs in page comunication

--- UniSabana/core/views.py
+++ UniSabana/core/views.py
@@ -20,6 +20,28 @@ class SingOutPageView(TemplateView):
     
 class TestGeneratorView(TemplateView):
     template_name = "core/testGenerator.html"
+    
+    print(1)
+    def get(self, request, *args, **kwargs):
+        print(2)
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        message = request.POST.get('prompt')
+        print(3)
+        try:
+            # Llamar a la función ask_openai para obtener una respuesta de OpenAI
+            response = self.ask_openai(message)
+
+            # Devolver la respuesta en un JSON
+            return JsonResponse({'message': message, 'response': response})
+        except Exception as e:
+            # Manejar cualquier excepción que pueda ocurrir y devolver un mensaje de error
+            return JsonResponse({'error': str(e)})
+    def ask_openai(self, message):
+        print(message)
+        return("hola")
+    
 
 class TestEditorView(TemplateView):
     template_name = "core/testEditor.html"
@@ -61,5 +83,4 @@ class ChatPageView(TemplateView):
         except Exception as e:
              # Manejar cualquier excepción que pueda ocurrir al interactuar con OpenAI
              return f"Error: {str(e)}"
-        
 
