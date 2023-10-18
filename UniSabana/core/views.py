d5e665eb7c03e8c4afc42b0185bdc1e2527e0ccb
sebastianrima <sebastianrima@unisabana.edu.co>
2023-10-18 16:44:09 -0500

    adding download of txt and formating of questions

--- UniSabana/core/views.py
+++ UniSabana/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import openai
 
-openai_api_key = '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'
+openai_api_key = 'a'
 openai.api_key = openai_api_key
 
 
@@ -21,27 +21,37 @@ class SingOutPageView(TemplateView):
 class TestGeneratorView(TemplateView):
     template_name = "core/testGenerator.html"
     
-    print(1)
     def get(self, request, *args, **kwargs):
-        print(2)
         return render(request, self.template_name)
 
     def post(self, request, *args, **kwargs):
         message = request.POST.get('prompt')
-        print(3)
         try:
             # Llamar a la función ask_openai para obtener una respuesta de OpenAI
             response = self.ask_openai(message)
-            print(response)
+            answerFixed = self.fixAnswer(response)
+            print(answerFixed)
             # Devolver la respuesta en un JSON
             print(4)
-            return JsonResponse({'message': message, 'response': response})
+            return JsonResponse({'message': message, 'response': answerFixed})
             
         except Exception as e:
             # Manejar cualquier excepción que pueda ocurrir y devolver un mensaje de error
             return JsonResponse({'error': str(e)})
+    def fixAnswer(self,answer):
+        lineas = answer.strip().split('\n')
+        formateado = []
+
+        for linea in lineas:
+            linea = linea.strip()
+            if linea.startswith("Pregunta"):
+                print(linea)
+                formateado.append("||" + linea[12:])
+            elif linea.startswith("A)") or linea.startswith("B)") or linea.startswith("C)") or linea.startswith("D)") or linea.startswith("ANSWER") or  linea.startswith(""):
+                formateado.append("||" + linea)
+
+        return "".join(formateado) + "||"
     def ask_openai(self, message):
-        print(message)
         try:
              response = openai.Completion.create(
                  model="text-davinci-003",
@@ -59,45 +69,43 @@ class TestGeneratorView(TemplateView):
         except Exception as e:
             response = """
                 Pregunta 1: ¿Cuál es el objetivo principal del substring mining en minería de datos?
-                ||A) Identificar patrones de búsqueda
-                ||B) Encontrar subcadenas en una secuencia
-                ||C) Clasificar grandes conjuntos de datos
-                ||D) Generar números pseudoaleatorios
-                ||ANSWER: B
-                ||
-                ||Pregunta 2: ¿Qué es la longitud mínima de un substring para que sea considerado relevante en el proceso de substring mining?
-                ||A) 1
-                ||B) 2
-                ||C) 3
-                ||D) 4
-                ||ANSWER: A
-                ||
-                ||Pregunta 3: En el contexto del substring mining, ¿qué es la "cobertura" de un substring?
-                ||A) El número de secuencias en las que aparece el substring
-                ||B) La longitud total del substring
-                ||C) El número de caracteres distintos en el substring
-                ||D) La probabilidad de ocurrencia del substring
-                ||ANSWER: A
-                ||
-                ||Pregunta 4: ¿Cuál es una técnica comúnmente utilizada en substring mining para encontrar subcadenas con alta ocurrencia?
-                ||A) Algoritmo de Dijkstra
-                ||B) Algoritmo de ordenación rápida
-                ||C) Algoritmo Apriori
-                ||D) Algoritmo de Euclides
-                ||ANSWER: C
-                ||
-                ||Pregunta 5: ¿Qué métrica se utiliza para evaluar la calidad de los resultados en substring mining?
-                ||A) Precisión
-                ||B) Recall
-                ||C) F1-score
-                ||D) Coeficiente de correlación
-                ||ANSWER: C
+                A) Identificar patrones de búsqueda
+                B) Encontrar subcadenas en una secuencia
+                C) Clasificar grandes conjuntos de datos
+                D) Generar números pseudoaleatorios
+                ANSWER: B
+                
+                Pregunta 2: ¿Qué es la longitud mínima de un substring para que sea considerado relevante en el proceso de substring mining?
+                A) 1
+                B) 2
+                C) 3
+                D) 4
+                ANSWER: A
+                
+                Pregunta 3: En el contexto del substring mining, ¿qué es la "cobertura" de un substring?
+                A) El número de secuencias en las que aparece el substring
+                B) La longitud total del substring
+                C) El número de caracteres distintos en el substring
+                D) La probabilidad de ocurrencia del substring
+                ANSWER: A
+                
+                Pregunta 4: ¿Cuál es una técnica comúnmente utilizada en substring mining para encontrar subcadenas con alta ocurrencia?
+                A) Algoritmo de Dijkstra
+                B) Algoritmo de ordenación rápida
+                C) Algoritmo Apriori
+                D) Algoritmo de Euclides
+                ANSWER: C
+                
+                Pregunta 5: ¿Qué métrica se utiliza para evaluar la calidad de los resultados en substring mining?
+                A) Precisión
+                B) Recall
+                C) F1-score
+                D) Coeficiente de correlación
+                ANSWER: C
                 """
              # Manejar cualquier excepción que pueda ocurrir al interactuar con OpenAI
             print("F")
             return response
-        
-        print(message)
         
 
 class TestEditorView(TemplateView):
@@ -127,7 +135,7 @@ class ChatPageView(TemplateView):
              response = openai.Completion.create(
                  model="text-davinci-003",
                  prompt=message,
-                 max_tokens=20,
+                 max_tokens=200,
                  n=1,
                  stop=None,
                  temperature=0.7,
